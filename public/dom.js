9af1b335809db31a0c48e492a8053b937789f5c6
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-04-09 08:15:44 -0500

    cambio de var por let o const

--- public/dom.js
+++ public/dom.js
@@ -1,61 +1,61 @@
-const prueba2 =()=>{
-    let textEmail= document.getElementById("email").value;
-    let textPassword = document.getElementById("password").value;
-    window.main.checkIn(textEmail, textPassword);
+const prueba2 = () => {
+  let textEmail = document.getElementById("email").value;
+  let textPassword = document.getElementById("password").value;
+  window.main.checkIn(textEmail, textPassword);
 }
-const btncheckIn= document.getElementById("buttonCheckIn");
-btncheckIn.addEventListener("click", prueba2 );
+const btncheckIn = document.getElementById("buttonCheckIn");
+btncheckIn.addEventListener("click", prueba2);
 
 function prueba() {
-    let textEmail1 = document.getElementById("email").value;
-    let textPassword1 = document.getElementById("password").value;
-    window.main.singIn(textEmail1, textPassword1);
+  let textEmail1 = document.getElementById("email").value;
+  let textPassword1 = document.getElementById("password").value;
+  window.main.singIn(textEmail1, textPassword1);
 
 }
-const btnSingInok=document.getElementById("buttonSingIn");
-btnSingInok.addEventListener("click", prueba );
-    
-function observer() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        document.getElementById("screen2").style.display = "block"
-        document.getElementById("screen1").style.display = "none"
-        console.log("existe usuario");
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // ...
-       
-      } else {
-        console.log ("no existe usuario");
-      }
-    });
-  }
-  observer();
+const btnSingInok = document.getElementById("buttonSingIn");
+btnSingInok.addEventListener("click", prueba);
 
-  function close() {
-    window.main.closeSession();
-  }
-const btnClose= document.getElementById("close");
+function observer() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      document.getElementById("screen2").style.display = "block"
+      document.getElementById("screen1").style.display = "none"
+      console.log("existe usuario");
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+
+    } else {
+      console.log("no existe usuario");
+    }
+  });
+}
+observer();
+
+function close() {
+  window.main.closeSession();
+}
+const btnClose = document.getElementById("close");
 btnClose.addEventListener("click", close);
 
-function googlebtn(){
-    window.main.googleSingIn();
+function googlebtn() {
+  window.main.googleSingIn();
 }
-    const btnGoogle = document.getElementById("google");
- btnGoogle.addEventListener("click", googlebtn);
+const btnGoogle = document.getElementById("google");
+btnGoogle.addEventListener("click", googlebtn);
 
- //esta funcion guarda los datos automaticamente//
+//esta funcion guarda los datos automaticamente//
 
-  function guardar (){
-    firebase.database().ref("usuariosGoogle").set({
-      nombre:"BlisS",
-      edad:"15",
-      sexo:"x"
-    })
-  }
+function guardar() {
+  firebase.database().ref("usuariosGoogle").set({
+    nombre: "BlisS",
+    edad: "15",
+    sexo: "x"
+  })
+}
--- public/main.js
+++ public/main.js
@@ -1,118 +1,126 @@
-window.main ={
-    checkIn:(textEmail, textPassword)=> {
+window.main = {
+  checkIn: (textEmail, textPassword) => {
     firebase
       .auth()
       .createUserWithEmailAndPassword(textEmail, textPassword)
-      .then(function(){
-        verific()
+      .then(function() {
+        verific();
       })
       .catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // ...
         console.log(errorCode, errorMessage);
       });
   },
-  
-   singIn:(textEmail1, textPassword1)=> {
+
+  singIn: (textEmail1, textPassword1) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(textEmail1, textPassword1)
       .catch(function(error) {
         document.getElementById("emailOK").innerHTML = error.message;
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // ...
-       console.log(errorMessage, errorCode )
+        console.log(errorMessage, errorCode);
       });
-    },
-    closeSession:()=>{
-      firebase.auth().signOut().then(function() {
+  },
+  closeSession: () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
         location.reload();
         // Sign-out successful.
         console.log("listo");
-      }).catch(function(error) {
+      })
+      .catch(function(error) {
         // An error happened.
         console.log(error);
       });
-    },
-    googleSingIn:()=>{
-  
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      console.log("ya la hisiste");
-      guardaDatos(result.user);  
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    }); 
-   
-   },
-   guardaDatos:(user)=>{
-    var usuario={
-      uid:user.uid,
-      nombre:user.displayName,
-      email:user.email,
-      foto:user.photoURL
-    }
-    firebase.database().ref("googleData")
-    .push(usuario)
+  },
+  googleSingIn: () => {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        console.log("ya la hisiste");
+        guardaDatos(result.user);
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // The email of the user's account used.
+        let email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        let credential = error.credential;
+        // ...
+      });
+  },
+  guardaDatos: user => {
+    const usuario = {
+      uid: user.uid,
+      nombre: user.displayName,
+      email: user.email,
+      foto: user.photoURL
+    };
+    firebase
+      .database()
+      .ref("googleData")
+      .push(usuario);
   }
-   
-   
-  }
-  
-  function verific(){
-    
-    var user = firebase.auth().currentUser;
-  user.sendEmailVerification().then(function() {
-    // Email sent.
-    console.log("enviando correo..");
-  }).catch(function(error) {
-    // An error happened.
-    console.log(error);
-  });
-  }
-  
-  
-  var db = firebase.firestore();
-  //Agregar comentario
-  function guardar(){
-    console.log("guardar")
-    let nombre = document.getElementById('nombre').value;
-    let fecha = document.getElementById('fecha').value;
-    let comentario = document.getElementById('comentario').value;
-    console.log(nombre, fecha, comentario)
-    db.collection("users").add({
+};
+
+function verific() {
+  const user = firebase.auth().currentUser;
+  user
+    .sendEmailVerification()
+    .then(function() {
+      // Email sent.
+      console.log("enviando correo..");
+    })
+    .catch(function(error) {
+      // An error happened.
+      console.log(error);
+    });
+}
+
+const db = firebase.firestore();
+//Agregar comentario
+function guardar() {
+  console.log("guardar");
+  let nombre = document.getElementById("nombre").value;
+  let fecha = document.getElementById("fecha").value;
+  let comentario = document.getElementById("comentario").value;
+  console.log(nombre, fecha, comentario);
+  db.collection("users")
+    .add({
       first: nombre,
       last: fecha,
       born: comentario
-      })
-      .then(function(docRef) {
+    })
+    .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
       // Refrescar pantalla por medio de strin limpios
-        document.getElementById("nombre").value = " ";
-        document.getElementById("fecha").value = " ";
-        document.getElementById("comentario").value = " ";
-      })
-      .catch(function(error) {
+      document.getElementById("nombre").value = " ";
+      document.getElementById("fecha").value = " ";
+      document.getElementById("comentario").value = " ";
+    })
+    .catch(function(error) {
       console.error("Error adding document: ", error);
-      });
-  }
-  //Leer documentos
-  var newPost = document.getElementById('newPost');
-  db.collection("users").onSnapshot((querySnapshot) => {
-      newPost.innerHTML = '';
-      querySnapshot.forEach((doc) => {
-          //console.log(`${doc.id} => ${doc.data().first}`);//
-      newPost.innerHTML += `
+    });
+}
+//Leer documentos
+let newPost = document.getElementById("newPost");
+db.collection("users").onSnapshot(querySnapshot => {
+  newPost.innerHTML = "";
+  querySnapshot.forEach(doc => {
+    //console.log(`${doc.id} => ${doc.data().first}`);//
+    newPost.innerHTML += `
       <div id="sub-menu">
     <div id="left-bar">
       <div class="heading">
@@ -134,7 +142,9 @@ window.main ={
         <div class="actions">
           <span class="heart"></span>
           <span class="comment" onclick="eliminar('${doc.id}')"></span>
-          <span class="share" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')"></span>
+          <span class="share" onclick="editar('${doc.id}','${
+      doc.data().first
+    }','${doc.data().last}','${doc.data().born}')"></span>
         </div>
       </div>
       <div class="content">
@@ -142,46 +152,49 @@ window.main ={
         
       </div>
     </div>
-    `
-      });
+    `;
   });
-  //borrar documentos
-  function eliminar(id){
-    db.collection("users").doc(id).delete().then(function() {
-        console.log("Document successfully deleted!");
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
+});
+//borrar documentos
+function eliminar(id) {
+  db.collection("users")
+    .doc(id)
+    .delete()
+    .then(function() {
+      console.log("Document successfully deleted!");
+    })
+    .catch(function(error) {
+      console.error("Error removing document: ", error);
     });
-    }
-  //editar documentos
-  function editar(id,nombre,fecha,comentario){
-      document.getElementById('nombre').value=nombre;
-      document.getElementById('fecha').value=fecha;
-      document.getElementById('comentario').value=comentario;
-      var boton=document.getElementById('boton');
-      boton.innerHTML = 'Editar';
-      boton.onclick = function(){
-      var washingtonRef = db.collection("users").doc(id);   
-  var nombre =document.getElementById('nombre').value;
-  var fecha=document.getElementById('fecha').value;
-  var comentario=document.getElementById('comentario').value;
-  return washingtonRef.update({
-      first: nombre,
-      last: fecha,
-      born: comentario
-  })
-  .then(function() {
-      console.log("Document successfully updated!");
-      boton.innerHTML = 'guardar';
-      document.getElementById('nombre').value = "";
-      document.getElementById('fecha').value= "";
-      document.getElementById('comentario').value= "";
-  })
-  .catch(function(error) {
-      // The document probably doesn't exist.
-      console.error("Error updating document: ", error);
-  }); 
-  }
-  }
-  
-  
+}
+//editar documentos
+function editar(id, nombre, fecha, comentario) {
+  document.getElementById("nombre").value = nombre;
+  document.getElementById("fecha").value = fecha;
+  document.getElementById("comentario").value = comentario;
+  let boton = document.getElementById("boton");
+  boton.innerHTML = "Editar";
+  boton.onclick = function() {
+    let washingtonRef = db.collection("users").doc(id);
+    let nombre = document.getElementById("nombre").value;
+    let fecha = document.getElementById("fecha").value;
+    let comentario = document.getElementById("comentario").value;
+    return washingtonRef
+      .update({
+        first: nombre,
+        last: fecha,
+        born: comentario
+      })
+      .then(function() {
+        console.log("Document successfully updated!");
+        boton.innerHTML = "guardar";
+        document.getElementById("nombre").value = "";
+        document.getElementById("fecha").value = "";
+        document.getElementById("comentario").value = "";
+      })
+      .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
+  };
+}
